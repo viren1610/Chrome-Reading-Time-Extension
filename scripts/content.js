11a5acc55de26f4948fa773eff18c302a2268141
Viren <virenplays@gmail.com>
2023-10-17 15:50:01 +0530

    adding logic

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,16 @@
-const article = document.querySelector('article')
+const article = document.querySelector("article");
 
-if(article){
+if (article) {
+  const text = article.textContent;
+  const wordmatchregexp = /[^\s]+/g;
+  const words = text.matchAll(wordMatchRegExp);
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  badge.classList.add("color-secondary-text", "type--caption")
+  badge.textContent = `⏱️ ${readingTime} min read`
+  const heading = article.querySelector("h1");
+  const date = article.querySelector("time")?.parentNode;
 
-    const word =''
+  (date ?? heading).insertAdjacentElement("afterend", badge);
 }
